b82bdba898668aa3376bea6373823632d18e330f
Stefania F <steff.20f@gmail.com>
2021-08-09 21:53:33 -0300

    creamos la app vue y establecimos lo básico para consumir la api

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>
+      Rick and Morty
+    </h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
@@ -15,12 +20,5 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,30 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    characters: [],
+    charactersFilter: []
   },
-  mutations: {
+  mutations: { //mutations modificans a los state
+    setCharacters(state, payload){
+      state.characters= payload
+    },
+    setCharactersFilter(state, payload){
+      state.charactersFilter = payload
+    }
   },
   actions: {
+    async getCharacters({commit}){ //recibe un commit para acceder a las mutations
+      try {
+        const url= 'https://rickandmortyapi.com/api/character';
+        const response= await fetch(url);
+        if(response.ok){
+          let t = await response.json();
+          console.log(t);
+        }
+      } catch (error) {
+        
+      }
+    } 
   },
   modules: {
   }
